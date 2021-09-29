0c88daf5247df28d48bf866f805fa256755222c6
AlexDev-py <aleks.filiov@yandex.ru>
2021-09-29 15:51:14 +0900

    - Новый интерфейс

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,29 +1,4 @@
-from PyQt5 import QtWidgets
-
+from . import icons_rc
 from .main import UiMainWindow
-from .search_view import UiSearchView
-from .library_view import UiLibraryView
-from .book import UiBook
 
-
-class View(QtWidgets.QFrame):
-    def __init__(self, container: QtWidgets.QFrame):
-        super(View, self).__init__(container)
-        container.layout().addWidget(self)
-        self.setupUi(self)
-        self.show()
-
-
-class SearchView(View, UiSearchView):
-    pass
-
-
-class LibraryView(View, UiLibraryView):
-    pass
-
-
-class BookWidget(View, UiBook):
-    pass
-
-
-__all__ = [UiMainWindow, SearchView, LibraryView]
+__all__ = [UiMainWindow]
