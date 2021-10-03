6bdeb12f112f7cbee3a584c4798a6c8d5a3aa2ff
AlexDev-py <aleks.filiov@yandex.ru>
2021-10-03 12:47:10 +0900

    - add docstring - style fix - add dirWithBooksBtn handler

--- ui_functions/library.py
+++ ui_functions/library.py
@@ -10,14 +10,24 @@ if ty.TYPE_CHECKING:
 
 
 def toggleFiltersPanel(main_window: Window) -> None:
-    def animation_finished():
-        main_window.toggleBooksFilterPanelBtn.setDisabled(False)
+    """
+    Открывает/закрывает меню фильтров.
+    :param main_window: Инстанс окна.
+    """
+
+    def animation_finished() -> None:
+        """
+        Вызывается, после окончания анимации.
+        """
+        main_window.toggleBooksFilterPanelBtn.setDisabled(False)  # Включает кнопку
         if end_value == 25:
             main_window.libraryFiltersFrame.hide()
 
-    width = main_window.libraryFiltersPanel.width()
+    width = main_window.libraryFiltersPanel.width()  # Ширина меню сейчас
+    # Конечная ширина меню 225-открытое 25-закрытое
     end_value = 225 if width == 25 else 25
 
+    # Отключаем кнопку на время анимации
     main_window.toggleBooksFilterPanelBtn.setDisabled(True)
 
     if end_value == 225:
@@ -26,14 +36,12 @@ def toggleFiltersPanel(main_window: Window) -> None:
     main_window.filters_menu_animation = QPropertyAnimation(
         main_window.libraryFiltersPanel, b"minimumWidth"
     )
-    main_window.filters_menu_animation.setDuration(500)
+    main_window.filters_menu_animation.setDuration(150)
     main_window.filters_menu_animation.setStartValue(width)
     main_window.filters_menu_animation.setEndValue(end_value)
     main_window.filters_menu_animation.setEasingCurve(QEasingCurve.InOutQuart)
     main_window.filters_menu_animation.start()
-    main_window.filters_menu_animation.finished.connect(
-        animation_finished
-    )  # Включаем кнопку по завершению анимации
+    main_window.filters_menu_animation.finished.connect(animation_finished)
 
     # Изменяем иконку кнопки
     last_icon = main_window.toggleBooksFilterPanelBtn.__dict__.get("_last_icon")
