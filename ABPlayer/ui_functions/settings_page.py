abeb4a48e2837e58c3d4ed234de6adb9673a6d50
AlexDev-py <aleks.filiov@yandex.ru>
2022-12-19 20:10:55 +0900

    update - При изменении папки с книгами, переносятся только файлы книг - После изменения папки с книгами база данных автоматически обновляется - Улучшено удаление старых директорий после изменения папки с книгами
    fix
    - Теперь при изменении папки с книгами приложение не зависает

--- ABPlayer/ui_functions/settings_page.py
+++ ABPlayer/ui_functions/settings_page.py
@@ -10,15 +10,125 @@ import os
 import pathlib
 import shutil
 import typing as ty
+from contextlib import suppress
 
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QFileDialog, QMessageBox
 from loguru import logger
 
 import styles
-from database import Config
+from database import Config, Books
+from tools import BaseWorker
 
 if ty.TYPE_CHECKING:
     from main_window import MainWindow
+    from PyQt5 import QtCore
+
+
+class SearchWorker(BaseWorker):
+    """
+    Класс реализующий поиск книги.
+    """
+
+    finished: QtCore.pyqtSignal = pyqtSignal()  # Перенос завершен
+
+    def __init__(self, main_window: MainWindow, new_path: str):
+        super(SearchWorker, self).__init__()
+        self.main_window, self.new_path = main_window, new_path
+
+    def connectSignals(self) -> None:
+        self.finished.connect(lambda: self.finish())
+
+    def worker(self) -> None:
+        logger.opt(colors=True).debug("Starting moving library.")
+        self.main_window.setLock(True)
+
+        books: dict[str, list[str]] = {}
+        for root, _, files in os.walk(os.environ["books_folder"]):
+            if ".abp" in files:
+                books[root.split(os.environ["books_folder"])[1][1:]] = [
+                    file
+                    for file in files
+                    if file.endswith(".mp3") or file in {"cover.jpg", ".abp"}
+                ]
+
+        logger.opt(colors=True).debug(f"{len(books)} books found")
+
+        # Копируем книги в новую директорию
+        for book_dir_name, files in books.items():
+            old_book_path = os.path.join(os.environ["books_folder"], book_dir_name)
+            new_book_path = pathlib.Path(self.new_path, book_dir_name)
+            logger.opt(colors=True).debug(
+                f"Copying <y>{old_book_path}</y> to <y>{new_book_path}</y>"
+            )
+            new_book_path.mkdir(parents=True, exist_ok=True)  # Создаем директорию книги
+            for file_name in files:
+                old_file_path = os.path.join(old_book_path, file_name)
+                new_file_path = os.path.join(new_book_path, file_name)
+                logger.opt(colors=True).trace(
+                    f"Copying <y>{old_file_path}</y> to <y>{new_file_path}</y>"
+                )
+                shutil.copyfile(old_file_path, new_file_path)  # Копируем
+                try:  # Пробуем удалить старый файл
+                    os.remove(old_file_path)
+                except Exception:
+                    logger.opt(colors=True).error(
+                        f"Unable to delete file <y>{old_file_path}</y>."
+                    )
+
+            logger.opt(colors=True).trace(
+                f"Copying <y>{old_book_path}</y> to <y>{new_book_path}</y> complete"
+            )
+
+            with suppress(Exception):  # Пробуем удалить старую директорию книги
+                os.rmdir(old_book_path)
+
+                book_dir_name = book_dir_name.replace("\\", "/")
+                if book_dir_name.endswith("/"):
+                    book_dir_name = book_dir_name[1:]
+
+                while "/" in book_dir_name:
+                    book_dir_name = book_dir_name[: book_dir_name.rfind("/")]
+                    os.rmdir(os.path.join(os.environ["books_folder"], book_dir_name))
+
+        old_books_folder = os.environ["books_folder"]
+        Config.update(
+            books_folder=self.new_path
+        )  # Обновляем настройки в бд и виртуальном окружении
+        logger.opt(colors=True).debug(
+            "Directory with books changed. "
+            f"<y>{old_books_folder}</y> -> <y>{os.environ['books_folder']}</y>"
+        )
+
+        logger.trace("Loading library")
+
+        db = Books(os.environ["DB_PATH"])
+        logger.trace("Dropping library table")
+        db.api.execute("DROP TABLE IF EXISTS books")
+        logger.trace("Creating library table")
+        db.create_table()
+        logger.trace("Library table created")
+
+        abp_files: list[str] = []
+        for root, _, files in os.walk(os.environ["books_folder"]):
+            if ".abp" in files:
+                abp_files.append(os.path.join(root, ".abp"))
+
+        logger.debug(f"{len(abp_files)} abp files found")
+
+        for abp in abp_files:
+            logger.trace(f"Loading {abp}")
+            book = Books.load_from_storage(abp)
+            db.insert(**book)
+
+        self.finished.emit()
+        self.main_window.setLock(False)
+
+    def finish(self) -> None:
+        self.main_window.stackedWidget.setCurrentWidget(self.main_window.settingsPage)
+        QMessageBox.information(
+            self.main_window, "Информация", "Папка успешно изменена"
+        )
 
 
 @logger.catch
@@ -43,49 +153,9 @@ def setDirWithBooks(main_window: MainWindow) -> None:
 
     main_window.openLoadingPage()  # Открываем страницу загрузки
 
-    file_paths = []  # Пути к файлам книг
-    roots = []  # Директории с книгами
-    for root, _, file_names in os.walk(os.environ["books_folder"]):
-        for file_name in file_names:
-            file_paths.append(
-                os.path.join(root.split(os.environ["books_folder"])[1], file_name)[1:]
-            )
-        roots.append(root)
-
-    # Копируем книги в новую директорию
-    for file_path in file_paths:
-        old_file_path = os.path.join(os.environ["books_folder"], file_path)
-        new_file_path = pathlib.Path(path, file_path)  # Новый путь к файлу
-        logger.opt(colors=True).trace(
-            f"Copying <y>{old_file_path}</y> to <y>{new_file_path}</y>"
-        )
-        if not new_file_path.exists():  # Создаем директорию книги
-            new_file_path.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copyfile(old_file_path, new_file_path)  # Копируем
-        try:  # Пробуем удалить старый файл
-            os.remove(old_file_path)
-        except Exception:
-            logger.opt(colors=True).exception(
-                f"Unable to delete file <y>{old_file_path}</y>."
-            )
-
-    for root in roots[::-1]:
-        try:  # Пробуем удалить старые директории книг
-            os.rmdir(root)
-        except Exception:
-            logger.opt(colors=True).exception(
-                f"Unable to delete directory <y>{root}</y>. "
-            )
-
-    old_books_folder = os.environ["books_folder"]
-    Config.update(books_folder=path)  # Обновляем настройки в бд и виртуальном окружении
-    logger.opt(colors=True).debug(
-        "Directory with books changed. "
-        f"<y>{old_books_folder}</y> -> <y>{os.environ['books_folder']}</y>"
-    )
-
-    main_window.stackedWidget.setCurrentWidget(main_window.settingsPage)
-    QMessageBox.information(main_window, "Информация", "Папка успешно изменена")
+    # Создаём и запускаем новый поток
+    main_window.searchWorker = SearchWorker(main_window, path)
+    main_window.searchWorker.start()
 
 
 def openDirWithBooks() -> None:
