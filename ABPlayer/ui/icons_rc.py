9f908a7a0778c4f5871b7020f45d2e986afe6adc
AlexDev-py <aleks.filiov@yandex.ru>
2022-12-20 23:06:18 +0900

    fix - style fix

--- ABPlayer/ui/icons_rc.py
+++ ABPlayer/ui/icons_rc.py
@@ -24710,7 +24710,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x7c\x21\x19\x8c\x53\
 "
 
-qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+qt_version = [int(v) for v in QtCore.qVersion().split(".")]
 if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
@@ -24718,10 +24718,17 @@ else:
     rcc_version = 2
     qt_resource_struct = qt_resource_struct_v2
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        rcc_version, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 qInitResources()
