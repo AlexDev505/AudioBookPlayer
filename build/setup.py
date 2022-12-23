13ee7be4d494b0d6a92dc90baff47df828b59e8a
AlexDev-py <aleks.filiov@yandex.ru>
2022-12-23 20:35:13 +0900

    Revert "b6 release"
    This reverts commit 27d0a411c268e9879d918fd35d9de067b9d6cca3.

--- build/setup.py
+++ build/setup.py
@@ -1,31 +1,29 @@
 """
 
     Сборка приложения в exe.
+    Для использования, дополнительно установите cx_Freeze используя
+    pip install -U cx_freeze
 
     Сборка проекта осуществляется в 2 этапа.
-    1. Сборка в exe. Осуществляется с помощью pyinstaller.
+    1. Сборка в exe. Осуществляется с помощью cx_Freeze.
     2. Сборка в exe установщик, осуществляемая при помощи nsis.
 
 """
-import os
+
 import re
-import shutil
-import sys
 
-import PyInstaller.__main__
+import cx_Freeze
 
-os.system(f"{sys.executable} {os.path.abspath('./updater_setup.py')}")
-
-__version__ = "1.0.0-beta.6"
-target_dir = "ABPlayer"
+__version__ = "1.0b5"
+target_dir = rf"ABPlayer"
 
 # Изменяем версию в main.py
-with open(r"..\ABPlayer\main.py", encoding="utf-8") as file:
+with open(r"../ABPlayer/main.py", encoding="utf-8") as file:
     text = file.read()
 text = re.sub(
     r'os.environ\["VERSION"] = ".+"', f'os.environ["VERSION"] = "{__version__}"', text
 )
-with open(r"..\ABPlayer\main.py", "w", encoding="utf-8") as file:
+with open(r"../ABPlayer/main.py", "w", encoding="utf-8") as file:
     file.write(text)
 
 # Изменяем версию в установщике
@@ -34,58 +32,82 @@ with open("installer.nsi") as file:
 text = re.sub(
     r'!define PRODUCT_VERSION ".+"', f'!define PRODUCT_VERSION "{__version__}"', text
 )
-# Добавляем вызов утилиты обновления
-if os.path.exists(r"sources\ABPUpdater.exe"):
-    if r'File "sources\ABPUpdater.exe"' not in text:
-        text = re.sub(
-            r'Section "AB Player" SEC01\n {2}SetOutPath "\$INSTDIR"',
-            r'Section "AB Player" SEC01\n  SetOutPath "$INSTDIR"\n\n  '
-            r'File "sources\\ABPUpdater.exe"\n  ExecWait "$INSTDIR\\ABPUpdater.exe"\n  '
-            r'Delete "$INSTDIR\\ABPUpdater.exe"\n',
-            text,
-            flags=re.MULTILINE,
-        )
-else:
-    if r'File "sources\ABPUpdater.exe"' in text:
-        text = re.sub(
-            r'\n\n {2}File "sources\\ABPUpdater.exe"\n'
-            r' {2}ExecWait "\$INSTDIR\\ABPUpdater.exe"\n'
-            r' {2}Delete "\$INSTDIR\\ABPUpdater.exe"\n',
-            r"",
-            text,
-            flags=re.MULTILINE,
-        )
 with open("installer.nsi", "w") as file:
     file.write(text)
 
-# Изменяем версию в version_file
-with open(r"sources\version_file") as file:
-    text = file.read()
-text = re.sub(
-    r"StringStruct\(u'(?P<name>(FileVersion)|(ProductVersion))', u'.+'\)",
-    rf"StringStruct(u'\g<name>', u'{__version__}')",
-    text,
+
+executables = [
+    cx_Freeze.Executable(
+        script=r"ABPlayer\run.py",  # Запускаемый файл
+        base="Win32GUI",  # Использует pythonw.exe
+        targetName="ABPlayer.exe",  # Имя exe
+        icon=r"interface\resources\icon.ico",
+    )
+]
+excludes = [
+    "test",
+    "tkinter",
+    "lib2to3",
+    "pydoc_data",
+    "pkg_resources",
+]  # Ненужные библиотеки
+zip_include_packages = [
+    "collections",
+    "encodings",
+    "importlib",
+    "json",
+    "click",
+    "ctypes",
+    "flask",
+    "logging",
+    "urllib",
+    "threading",
+    "email",
+    "http",
+    "html",
+    "distutils",
+    "multiprocessing",
+    "xmlrpc",
+    "wsgiref",
+    "werkzeug",
+    "xml",
+    "werkzeug",
+    "itsdangerous",
+    "sqlite3_api",
+    "sqlite3",
+    "openpyxl",
+    "et_xmlfile",
+    "idna",
+    "asyncio",
+    "requests",
+    "unittest",
+    "urllib3",
+    "concurrent",
+    "packaging",
+    "loguru",
+]  # Библиотеки, помещаемые в архив
+
+cx_Freeze.setup(
+    name="AB Player",
+    options={
+        "build_exe": {
+            "packages": [
+                "PyQt5",
+                "selenium",
+                "requests",
+                "eyed3",
+                "loguru",
+            ],  # Библиотеки
+            "excludes": excludes,
+            "zip_include_packages": zip_include_packages,
+        },
+        "build": {"build_exe": target_dir},  # Место назначения
+    },
+    version=__version__,
+    description="Приложение для Windows,"
+    " которое позволит вам бесплатно слушать и скачивать аудио книги.",
+    author="AlexDev",
+    executables=executables,
 )
-with open(r"sources\version_file", "w") as file:
-    file.write(text)
 
-shutil.rmtree("ABPlayer", ignore_errors=True)
-
-PyInstaller.__main__.run(
-    [
-        "../abplayer/run.py",
-        "-D",
-        "-n=ABPlayer",
-        "--version-file=version_file",
-        "--icon=icon.ico",
-        "--distpath=.",
-        "--workpath=abp_temp",
-        "--specpath=sources",
-        "-y",
-        "--clean",
-        "-w",
-        "--hiddenimport=plyer.platforms.win.notification",
-    ]
-)
-
-shutil.rmtree("abp_temp", ignore_errors=True)
+# python setup.py build  # Сборка exe
