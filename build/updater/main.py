6cbb2ee4ed0c8634a1f26efe28843f079a0cbc0a
AlexDev-py <aleks.filiov@yandex.ru>
2022-12-23 18:39:50 +0900

    fix - Выход из приложения

--- build/updater/main.py
+++ build/updater/main.py
@@ -1,11 +1,12 @@
 import os
+import sys
 
 os.environ["APP_DIR"] = os.path.join(os.environ["LOCALAPPDATA"], "AudioBookPlayer")
 os.environ["DB_PATH"] = os.path.join(os.environ["APP_DIR"], "database.sqlite")
 os.environ["DEFAULT_BOOKS_FOLDER"] = os.path.join(os.environ["APP_DIR"], "Книги")
 
 if not os.path.exists(os.environ["DB_PATH"]):
-    exit()
+    sys.exit()
 
 from PyQt5.QtWidgets import QApplication  # noqa
 from update_window import UpdateWindow  # noqa
