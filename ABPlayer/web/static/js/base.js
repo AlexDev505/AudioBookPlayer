850ef47e5509dfe3ec42a3161bd13a490a5d8da4
AlexDev505 <aleks.filiov@yandex.ru>
2023-09-08 22:52:09 +0700

    interface base

--- ABPlayer/web/static/js/base.js
+++ ABPlayer/web/static/js/base.js
@@ -31,47 +31,12 @@ function getHttpRequestObject()
     return xmlHttpRequst;
 }
 
-// Does the AJAX call to URL specific with rest of the parameters
-function doAjax(url, method, responseHandler, data)
-{
-    // Set the variables
-    url = url || "";
-    method = method || "GET";
-    async = true;
-    data = data || {};
-    data.token = window.token;
-
-    if(url == "") {
-        alert("URL can not be null/blank");
-        return false;
-    }
-    var xmlHttpRequest = getHttpRequestObject();
-
-    // If AJAX supported
-    if(xmlHttpRequest != false) {
-        xmlHttpRequest.open(method, url, async);
-        // Set request header (optional if GET method is used)
-        if(method == "POST")  {
-            xmlHttpRequest.setRequestHeader("Content-Type", "application/json");
-        }
-        // Assign (or define) response-handler/callback when ReadyState is changed.
-        xmlHttpRequest.onreadystatechange = responseHandler;
-        // Send data
-        xmlHttpRequest.send(JSON.stringify(data));
-    }
+sideMenu = document.getElementById("side-menu")
+var menu_opened = true
+function toggleMenu() {
+    if (menu_opened)
+        sideMenu.classList.add("collapsed")
     else
-    {
-        alert("Please use browser with Ajax support.!");
-    }
-}
-
-function getHttpHost(host) {
-    return `${(host.includes("localhost"))? 'http' : 'https'}` + '://' + host
-}
-function getWsHost(host) {
-    return `${(host.includes("localhost"))? 'ws' : 'wss'}` + '://' + host
-}
-function utcNow() {
-    var d = new Date();
-    return (d.getTime() + (d.getTimezoneOffset() * 60000)) / 1000
+        sideMenu.classList.remove("collapsed")
+    menu_opened = !menu_opened
 }
