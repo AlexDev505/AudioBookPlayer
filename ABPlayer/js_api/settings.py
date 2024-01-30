5d7514eebeec5f04e4d4b7a2e04267e47936090d
AlexDev505 <aleks.filiov@yandex.ru>
2024-01-31 00:33:07 +0900

    - checking for updates - updating app - first build - installer and updater

--- ABPlayer/js_api/settings.py
+++ ABPlayer/js_api/settings.py
@@ -2,16 +2,21 @@ from __future__ import annotations
 
 import os
 import shutil
+import sys
+import time
 import typing as ty
 from pathlib import Path
+from subprocess import Popen
 
+import requests
 import webview
 from loguru import logger
+from orjson import orjson
 
 import config
 from database import Database
 from models.book import Book
-from .js_api import JSApi, JSApiError
+from .js_api import JSApi, JSApiError, ConnectionFailedError
 
 
 if ty.TYPE_CHECKING:
@@ -165,7 +170,101 @@ class SettingsApi(JSApi):
 
         return self.make_answer(dict(removed_books_count=removed_books_count))
 
+    def check_for_updates(self):
+        logger.opt(colors=True).debug("request: <r>check for updates</r>")
+
+        if isinstance(last_release := self._get_last_release(), JSApiError):
+            return self.error(last_release)
+
+        last_version = last_release["tag_name"]
+        if last_version == os.environ["VERSION"]:
+            logger.debug("The same version is installed now")
+            return self.make_answer(False)
+
+        return self.make_answer(
+            dict(version=last_version, url=last_release["html_url"])
+        )
+
+    @staticmethod
+    def _get_last_release() -> dict | JSApiError:
+        logger.debug("Getting last release")
+        try:
+            response = requests.get(
+                "https://api.github.com/repos/AlexDev505/AudioBookPlayer/releases"
+            )
+            if response.status_code != 200:
+                raise requests.RequestException(
+                    f"Response status code: {response.status_code}"
+                )
+        except requests.exceptions.RequestException as err:
+            return ConnectionFailedError(err=f"{type(err).__name__}: {err}")
+
+        releases = orjson.loads(response.text)
+        last_release = releases[0]
+        logger.opt(colors=True).debug(
+            f"Last release: <y>{last_release['html_url']}</y>"
+        )
+        return last_release
+
+    def update_app(self):
+        logger.opt(colors=True).debug("request: <r>update appy</r>")
+
+        if isinstance(last_release := self._get_last_release(), JSApiError):
+            return self.error(last_release)
+
+        last_version = last_release["tag_name"]
+        updater_file_name = f"ABPlayerUpdate.{last_version}.exe"
+        for asset in last_release["assets"]:
+            if updater_file_name == asset["name"]:
+                updater_url = asset["browser_download_url"]
+                break
+        else:
+            logger.opt(colors=True).error(
+                f"file <y>{updater_file_name}</y> not found in release assets"
+            )
+            return self.error(UpdateFileNotFound())
+
+        updater_path = os.path.join(os.environ["APP_DIR"], updater_file_name)
+        logger.opt(colors=True).debug(
+            f"downloading updater file. <y>url={updater_url} path={updater_path}</y>"
+        )
+        if not self._download_updater(updater_url, updater_path):
+            return self.error(ConnectionFailedError())
+
+        logger.info("closing app")
+        self._window.destroy()
+        Popen([sys.executable, f"--run-update={updater_path}"])
+
+    def _download_updater(
+        self, updater_url: str, updater_path: str, _retries: int = 0
+    ) -> bool:
+        try:
+            response = requests.get(updater_url)
+            if response.status_code != 200:
+                logger.error(
+                    "updater downloading failed. "
+                    f"Response status code {response.status_code}"
+                )
+                return False
+            with open(updater_path, "wb") as file:
+                file.write(response.content)
+            return True
+        except IOError as err:
+            logger.error(
+                f"Updater downloading failed on {_retries} retry. "
+                f"{type(err).__name__}: {err}"
+            )
+            if _retries == 3:
+                return False
+            time.sleep(10 * _retries)
+            return self._download_updater(updater_url, updater_path, _retries + 1)
+
 
 class RequestCanceled(JSApiError):
     code = 7
     message = "Операция отменена"
+
+
+class UpdateFileNotFound(JSApiError):
+    code = 8
+    message = "Файл обновления не найден"
