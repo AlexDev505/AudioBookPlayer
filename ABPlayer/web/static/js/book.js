5b773e5a8b329411446440043e19ba74dd94b43f
AlexDev505 <aleks.filiov@yandex.ru>
2024-10-04 23:10:35 +0700

    - player ui - transferring data about listening to a book to the client part - styles of filter-by-section scrollbars fixed - requirements updates

--- ABPlayer/web/static/js/book.js
+++ ABPlayer/web/static/js/book.js
@@ -10,7 +10,7 @@ page("book-page").onHide = function() {
 
 
 function loadBookData(bid) {
-    pywebview.api.book_by_bid(bid).then((resp) => {
+    pywebview.api.book_by_bid(bid, true).then((resp) => {
         if (resp.status != "ok") {showError(resp.message); return}
         document.querySelector("#book-page-content .book-title").innerHTML = resp.data.name
         document.querySelector("#book-page-content .book-listening-progress").innerHTML = `${resp.data.listening_progress} прослушано`
@@ -25,6 +25,36 @@ function loadBookData(bid) {
         } else document.querySelector("#book-page-content .book-series").style = "display: none"
         document.querySelector("#book-page-content .book-driver").innerHTML = resp.data.driver
         document.querySelector("#book-page-content .book-description").innerHTML = resp.data.description
+        document.getElementById("player-controls").style = "display: none"
+        document.getElementById("player-downloading-required").style = "display: none"
+        document.getElementById("player-downloading").style = "display: none"
+        if (resp.data.downloaded) {
+            document.getElementById("player-controls").style = "display: flex"
+            document.getElementById("player").classList.remove("not-available")
+            html = ""
+            for (item of resp.data.items) {
+                html += `<div class="book-item">
+                  <span class="title">${item.title}</span>
+                  <span class="time">00:00 / ${timeView(item.end_time - item.start_time)}</span>
+                </div>`
+            }
+            document.getElementById("items-container").innerHTML = html
+        } else {
+            document.getElementById("player").classList.add("not-available")
+            if (resp.data.downloading) document.getElementById("player-downloading").style = "display: block"
+            else {
+                document.getElementById("player-downloading-required").style = "display: block"
+                document.getElementById("download-book-btn").onclick = function() {
+                    startDownloading(this, resp.data.bid, resp.data.name)
+                    document.getElementById("player-downloading-required").style = "display: none"
+                    document.getElementById("player-downloading").style = "display: block"
+                }
+            }
+        }
         document.getElementById("book-loading").style = "display: none;"
     })
+}
+
+function timeView(time) {
+    return `${String(Math.floor(time/60)).padStart(2, '0')}:${String(time%60).padStart(2, '0')}`
 }
