707e39a5df11f844f084af7594e9b933936c4ddd
AlexDev505 <aleks.filiov@yandex.ru>
2026-05-17 09:58:05 +0700

    - akniga driver improved

--- ABPlayer/drivers/akniga.py
+++ ABPlayer/drivers/akniga.py
@@ -1,17 +1,28 @@
+import os
 import re
+import sys
 import time
 import typing as ty
 from abc import ABC, abstractmethod
 from contextlib import suppress
 
+import orjson
+import requests
 import webview
 from bs4 import BeautifulSoup
-from loguru import logger
 from models.book import Book, BookItem, BookItems
 
 from .base import Driver
 from .downloaders import MergedM3U8Downloader
 from .tools import find_in_soup, safe_name
+
+if getattr(sys, "frozen", False):
+    DRIVERS_DIR = os.path.join(getattr(sys, "_MEIPASS"))
+else:
+    DRIVERS_DIR = os.path.dirname(__file__)
+
+DECRYPT_SCRIPT = os.path.join(DRIVERS_DIR, "bin/akniga_decrypt.js")
+
 
 """
 On the AKniga website, book information is loaded dynamically via JavaScript.
@@ -21,90 +32,69 @@ Therefore, to get complete book data, we need a JavaScript execution environment
 
 class BaseJsApi(ABC):
     @abstractmethod
-    def get_book_data(self, url: str) -> dict:
-        """
-        Method that returns dynamically loaded book data.
-        {
-            "author": <str>,
-            "titleonly": <str>,  # clean book title
-            "items": [{
-                "file": <int>,  # file number
-                "title": <str>,  # chapter title
-                "time_from_start": <str>,  # start time
-                "time_finish": <str>,  # end time
-            }, ...],
-            "m3u8": <str>,  # link to m3u8 file
-        }
-        """
+    def decrypt_hres(self, hres: str) -> str:
+        """Decrypts URL to audio file"""
 
 
 class PyWebViewJsApi(BaseJsApi):
     """
-    JavaScript execution environment using Pywebview.
-    Creates a hidden window, loads the book page there, and retrieves data from it.
+    JavaScript execution environment using Pywebview
+    Uses already running pywebview window to execute JavaScript
     """
 
-    def __init__(self):
-        self._window: webview.Window | None = None
-        self._result = {}
-        self._active = False
-
-    def get_book_data(self, url: str) -> dict:
-        self._window = webview.create_window("", url=url, hidden=True)
-        # Execute `self._get_book_data` after the page loads
-        self._window.events.loaded += self._get_book_data
-        self._active = True
-        if (
-            len(webview.windows) == 1
-        ):  # Currently, there are no running pywebview windows
-            webview.start()
-        else:
-            while self._active:
-                time.sleep(1)
-        return self._result
-
-    def _get_book_data(self) -> None:
-        for i in range(5):
-            self._result = self._window.evaluate_js(
-                """
-                function get_data() {
-                    return {
-                        "author": bookData[page_bid].author,
-                        "titleonly": bookData[page_bid].titleonly,
-                        "items": bookData[page_bid].items,
-                        "m3u8": hls[page_bid].url
-                    }
-                }
-                get_data()
-                """
-            )
-            if self._result:
-                break
-            time.sleep(5)
-        else:
-            logger.error(
-                f"getting book data failed. url={self._window.get_current_url}"
-            )
-            self._window.load_url(self._window.get_current_url())
-            return
-
-        self._window.destroy()
-        self._active = False
+    def decrypt_hres(self, hres: str) -> str:
+        _window = webview.windows[0]
+        with open(DECRYPT_SCRIPT, encoding="utf-8") as f:
+            script = f.read()
+        script += f"\n\nplh.getHres('{hres}')"
+        return _window.evaluate_js(script)
 
 
 class AKniga(Driver):
     site_url = "https://akniga.org"
     downloader_factory = MergedM3U8Downloader
 
+    PER_PAGE = 12
+    HEADERS = {
+        "origin": site_url,
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36",
+    }
+    TOKEN_URL = "https://akniga.org/ajax/player/token"
+    BOOK_URL = "https://akniga.org/ajax/b/{bid}"
+
     def __init__(self, js_api: ty.Type[BaseJsApi] = PyWebViewJsApi):
         super().__init__()
         self.js_api = js_api
 
     def get_book(self, url: str) -> Book:
-        page = self.get_page(url)
-        soup = BeautifulSoup(page.content, "html.parser")
+        session = requests.Session()
+        page = session.get(url, headers=self.HEADERS)
 
-        book_data = self.js_api().get_book_data(url)
+        sk = re.search(r"LIVESTREET_SECURITY_KEY = '(.+?)'", page.text).group(1)
+        bid = re.search(r'data-bid="(.+?)"', page.text).group(1)
+        token = session.post(
+            self.TOKEN_URL,
+            headers=self.HEADERS,
+            data={
+                "security_ls_key": sk,
+                "bid": bid,
+                "ts": int(time.time() * 1000),
+            },
+        ).json()["token"]
+
+        book_data = session.post(
+            self.BOOK_URL.format(bid=bid),
+            headers=self.HEADERS,
+            data={
+                "security_ls_key": sk,
+                "bid": bid,
+                "token": token,
+                "hls": True,
+            },
+        ).json()
+        m3u8 = self.js_api().decrypt_hres(book_data["hres"])
+
+        soup = BeautifulSoup(page.content, "html.parser")
 
         author = book_data.get("author") or _("unknown_author")
         name = book_data["titleonly"]
@@ -137,11 +127,11 @@ class AKniga(Driver):
         reader = find_in_soup(soup, "a.link__reader span")
         preview = soup.select_one("div.book--cover img").attrs["src"]
         items = BookItems()
-        for item in book_data["items"]:
+        for item in orjson.loads(book_data["items"]):
             items.append(
                 BookItem(
-                    file_url=book_data["m3u8"],
-                    file_index=item["file"] - 1,
+                    file_url=m3u8,
+                    file_index=0,
                     title=safe_name(item["title"]),
                     start_time=item["time_from_start"],
                     end_time=item["time_finish"],
@@ -197,12 +187,10 @@ class AKniga(Driver):
         self, query: str, limit: int = 10, offset: int = 0
     ) -> list[Book]:
         books = []
-        page_number = 1
+        page_number = offset // self.PER_PAGE + 1
+        offset %= self.PER_PAGE
 
-        while True:
-            if len(books) == limit:
-                break
-
+        while len(books) < limit:
             url = self.site_url + f"/search/books/page{page_number}/?q={query}"
 
             page = self.get_page(url)
@@ -216,12 +204,8 @@ class AKniga(Driver):
                 break
 
             if offset:
-                if offset > len(elements):
-                    offset -= len(elements)
-                    elements.clear()
-                else:
-                    elements = elements[offset:]
-                    offset = 0
+                elements = elements[offset:]
+                offset = 0
 
             for card in elements:
                 if book := self._parse_book_card(card):
@@ -236,28 +220,21 @@ class AKniga(Driver):
     def _parse_book_card(self, card: BeautifulSoup) -> Book | None:
         with suppress(AttributeError, KeyError, TypeError):
             url = card.select_one("div.article--cover > a").attrs["href"]
-            preview = card.select_one("div.article--cover > a > img").attrs[
-                "src"
-            ]
+            preview = card.select_one("div.article--cover > a img").attrs["src"]
             author = find_in_soup(
                 card,
-                r'span.link__action--author> svg:has(use[xlink\:href="#author"]) ~ a',
+                "span.link__action--author> a[href*='author']",
                 _("unknown_author"),
             )
             try:
-                name = card.select_one("div.article--cover > a > img").attrs[
+                name = card.select_one("div.article--cover > a img").attrs[
                     "alt"
                 ]
             except AttributeError:
-                name = (
-                    card.select_one(".caption__article-main")
-                    .text.replace(f"{author} - ", "")
-                    .strip()
-                )
+                name = card.select_one(".caption__article-main").text
+            name = name.replace(f"{author} – ", "").strip()
             reader = find_in_soup(
-                card,
-                "span.link__action--author> "
-                r'svg:has(use[xlink\:href="#performer"]) ~ a',
+                card, "span.link__action--author> a[href*='performer']"
             )
             duration = find_in_soup(card, "span.link__action--label--time")
             series_name = number_in_series = ""
